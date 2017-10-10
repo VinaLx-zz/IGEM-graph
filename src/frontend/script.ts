f4acda2161b02e8214ed9e6ab3835c63fe868cda
VinaLx <vinaleux@gmail.com>
2017-10-11 00:34:32 +0800

    basic idea work

--- src/frontend/script.ts
+++ src/frontend/script.ts
@@ -8,13 +8,18 @@ interface GraphLink {
 }
 
 interface GraphNode extends d3.SimulationNodeDatum {
-    id: string;
+    name: string;
     group: number;
 }
 
 interface Graph {
     nodes: GraphNode[];
     links: GraphLink[];
+}
+
+interface InputJson {
+    nodes: Array<{ name: string, group: number }>;
+    links: Array<{ source: number, target: number, value: number }>;
 }
 
 const svg = d3.select("svg");
@@ -24,19 +29,35 @@ const height = Number(svg.attr("height"));
 const color = d3.scaleOrdinal(d3.schemeCategory20);
 
 const simulation: any = d3.forceSimulation<GraphNode>()
-    .force("link", d3.forceLink().id((d: any) => d.id))
+    .force("link", d3.forceLink().id((d: any) => d.name))
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2)) as d3.Simulation<GraphNode, GraphLink>;
 
-d3.json("miserables.json", (error, graph: Graph) => {
-    if (error) { throw error; }
+function fromInputJson(j: InputJson): Graph {
+    const nodes = j.nodes as GraphNode[];
+    const links = Array<GraphLink>();
+    for (const { source, target, value } of j.links) {
+        links.push({
+            source: nodes[source].name,
+            target: nodes[target].name,
+            value,
+        });
+    }
+    return { links, nodes };
+}
+
+d3.json("graph2009.json", (error, json: InputJson) => {
+    if (error) {
+        throw error;
+    }
+    const graph = fromInputJson(json);
 
     const link = svg.append("g")
         .attr("class", "links")
         .selectAll("line")
         .data(graph.links)
         .enter().append("line")
-        .attr("stroke-width", (d: any) => Math.sqrt(d.value));
+        .attr("stroke-width", (d) => Math.sqrt(d.value));
 
     const node = svg.append("g")
         .attr("class", "nodes")
@@ -44,13 +65,13 @@ d3.json("miserables.json", (error, graph: Graph) => {
         .data(graph.nodes)
         .enter().append("circle")
         .attr("r", 5)
-        .attr("fill", (d: any) => color(d.group))
+        .attr("fill", (d) => color(String(d.group)))
         .call(d3.drag<Element, GraphNode>()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended));
 
-    // node.append("title").text((d: any) => d.id);
+    node.append("title").text((d) => d.name);
 
     simulation.nodes(graph.nodes)
         .on("tick", () => {
@@ -60,8 +81,8 @@ d3.json("miserables.json", (error, graph: Graph) => {
                 .attr("x2", (d: any) => d.target.x)
                 .attr("y2", (d: any) => d.target.y);
             node
-                .attr("cx", (d: GraphNode) => d.x)
-                .attr("cy", (d: GraphNode) => d.y);
+                .attr("cx", (d: any) => d.x)
+                .attr("cy", (d: any) => d.y);
         });
 
     simulation.force/*<d3.ForceLink<{}, GraphLink>>*/("link").links(graph.links);
