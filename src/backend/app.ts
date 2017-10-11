dc27d120b5effebfe13f1b982625ff8d514cd407
VinaLx <vinaleux@gmail.com>
2017-10-11 18:34:37 +0800

    add readme.md

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -12,5 +12,5 @@ app.use(express.static(path.join(__dirname, "../static")));
 app.use(express.static(__dirname));
 
 app.listen(port, "0.0.0.0", () => {
-    console.log(`listening on port ${port}`);
+    console.log(`listening on http://localhost:${port}`);
 });
