29d473d0d5320f870527a8e1ab5b67b05b489461
VinaLx <vinaleux@gmail.com>
2017-10-14 21:50:36 +0800

    add url to teams

--- src/frontend/script.ts
+++ src/frontend/script.ts
@@ -1,4 +1,4 @@
-/* tslint:disable:interface-name no-console no-var-requires */
+/* tslint:disable:interface-name no-console no-var-requires space-before-function-paren */
 import * as d3 from "d3";
 import * as Const from "./constants";
 
@@ -13,6 +13,7 @@ interface GraphLink {
 
 interface GraphNode extends d3.SimulationNodeDatum {
     id: number;
+    url: string;
     name: string;
     group: number;
 }
@@ -23,7 +24,7 @@ interface Graph {
 }
 
 interface InputJson {
-    nodes: Array<{ name: string, group: number }>;
+    nodes: Array<{ name: string, group: number, url: string }>;
     links: Array<{ source: number, target: number, value: number }>;
 }
 
@@ -31,7 +32,6 @@ const svg = d3.select("svg");
 const width = Number(svg.attr("width"));
 const height = Number(svg.attr("height"));
 
-console.log(d3.schemeCategory20);
 const color = d3.scaleOrdinal(d3.schemeCategory20);
 
 function InitForceSimulation([cx, cy]: [number, number]) {
@@ -47,11 +47,7 @@ function InputJsonToGraph(j: InputJson): Graph {
     const nodes = Array<GraphNode>();
     const links = j.links;
     for (let i = 0; i < j.nodes.length; ++i) {
-        nodes.push({
-            group: j.nodes[i].group,
-            id: i,
-            name: j.nodes[i].name,
-        });
+        nodes.push({ ...j.nodes[i], id: i });
     }
     return { links, nodes };
 }
@@ -99,26 +95,34 @@ function ElementAjacencyArray<E extends d3.BaseType, PE extends d3.BaseType, PD>
 
 function InitNodeSelection<E extends d3.BaseType>(
     selectedSVG: DirectSelect<E>, nodes: GraphNode[]) {
-    const node = selectedSVG.append("g").attr("class", "nodes")
-        .selectAll("circle")
-        .data(nodes).enter()
-        .append("a")
-        .attr("href", "http://www.example.com").attr("target", "_blank")
+    const circleNode = selectedSVG.append("g").attr("class", "nodes")
+        .selectAll("a").data(nodes).enter()
+        .append("a").attr("href", (d) => d.url).attr("target", "_blank")
         .append("circle")
-        .attr("r", 7).attr("fill", (d) => color(String(d.group)))
+        .attr("r", 7).attr("fill", (d) => d3.schemeCategory20[d.group])
         .attr("data-toggle", "tooltip")
         .attr("title", (d) => d.name)
         .call(d3.drag<any, GraphNode>()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended));
-    // node.append("title").text((d) => d.name);
-    return node;
+
+    DeleteLinksFromTopics(circleNode);
+    return circleNode;
+}
+
+function DeleteLinksFromTopics<E extends d3.BaseType, PE extends d3.BaseType, PD>(
+    nodes: d3.Selection<E, GraphNode, PE, PD>) {
+    nodes.filter((d) => d.group === 0).each(function (d) {
+        const p = (this as Node).parentNode as Node;
+        const pp = p.parentNode as Node;
+        pp.replaceChild(this as Node, p);
+    });
 }
 
 function NodeHoverEffect<E extends d3.BaseType, PE extends d3.BaseType, PD>(
     nodes: d3.Selection<E, GraphNode, PE, PD>, aa: E[][]) {
-    nodes.on("mouseover", function(d) {
+    nodes.on("mouseover", function (d) {
         d3.select(this)
             .transition()
             .duration(Const.kNodeTransDuration)
@@ -128,7 +132,7 @@ function NodeHoverEffect<E extends d3.BaseType, PE extends d3.BaseType, PD>(
             .duration(Const.kNodeTransDuration)
             .style("stroke", "#000")
             .style("stroke-width", "2px");
-    }).on("mouseout", function(d) {
+    }).on("mouseout", function (d) {
         d3.select(this)
             .transition()
             .duration(Const.kNodeTransDuration)
@@ -156,28 +160,35 @@ function SetSimulationLinks(
     linkForce.links(links).distance((l) => 50 / l.value);
 }
 
+type CommonSelect<D> = d3.Selection<d3.BaseType, D, d3.BaseType, {}>;
+
+function ConnectModelAndView(
+    sim: d3.Simulation<GraphNode, GraphLink>,
+    nodes: CommonSelect<GraphNode>, links: CommonSelect<GraphLink>) {
+    simulation.on("tick", () => {
+        links
+            .attr("x1", (d: any) => d.source.x)
+            .attr("y1", (d: any) => d.source.y)
+            .attr("x2", (d: any) => d.target.x)
+            .attr("y2", (d: any) => d.target.y);
+        nodes
+            .attr("cx", (d: any) => d.x)
+            .attr("cy", (d: any) => d.y);
+    });
+}
+
 function DrawForceGraph(graph: Graph) {
-    const link = InitLinkSelection(svg, graph.links);
-    const node = InitNodeSelection(svg, graph.nodes);
+    const links = InitLinkSelection(svg, graph.links);
+    const nodes = InitNodeSelection(svg, graph.nodes);
 
-    const ajacencyArray = ElementAjacencyArray(node.size(), link);
+    const ajacencyArray = ElementAjacencyArray(nodes.size(), links);
 
-    NodeHoverEffect(node, ajacencyArray);
+    NodeHoverEffect(nodes, ajacencyArray);
 
     SetSimulationNodes(simulation, graph.nodes);
     SetSimulationLinks(simulation, graph.links);
 
-    simulation.nodes(graph.nodes)
-        .on("tick", () => {
-            link
-                .attr("x1", (d: any) => d.source.x)
-                .attr("y1", (d: any) => d.source.y)
-                .attr("x2", (d: any) => d.target.x)
-                .attr("y2", (d: any) => d.target.y);
-            node
-                .attr("cx", (d: any) => d.x)
-                .attr("cy", (d: any) => d.y);
-        });
+    ConnectModelAndView(simulation, nodes, links);
 
     EnableSVGTooltip();
 }
@@ -191,7 +202,7 @@ function EnableSVGTooltip() {
     });
 }
 
-d3.json("graph2009.json", (error, json: InputJson) => {
+d3.json("graph2009-url.json", (error, json: InputJson) => {
     if (error) {
         throw error;
     }
