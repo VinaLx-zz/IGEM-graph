0f70eec46ebe36d16c338f88e53abc4fcb902e66
VinaLx <vinaleux@gmail.com>
2017-10-16 16:31:09 +0800

    encapsulate size parameters

--- src/frontend/constants.ts
+++ src/frontend/constants.ts
@@ -1,20 +1,19 @@
 /* tslint:disable: no-namespace */
 
 namespace Const {
-
-    export const kRadiusExpand = 1.5;
-    export const kNodeTransDuration = 250;
-
     const kLinkDistanceF = 0.05;
-    export let kNodeRadius = -1;
-
     const kNodeRadiusF = 0.013;
-    export let kLinkDistance = -1;
 
-    export function InitSizes(width: number, height: number) {
-        const min = width < height ? width : height;
-        kNodeRadius = kNodeRadiusF * min;
-        kLinkDistance = kLinkDistanceF * min;
+    export class Params {
+        public nodeRadius: number;
+        public linkDistance: number;
+        public nodeTransDuration: number = 250;
+        public radiusExpand: number = 1.5;
+        constructor([width, height]: [number, number]) {
+            const min = width < height ? width : height;
+            this.nodeRadius = kNodeRadiusF * min;
+            this.linkDistance = kLinkDistanceF * min;
+        }
     }
 
     const ordinal = d3.scaleOrdinal(d3.schemeCategory20);
--- src/frontend/script.ts
+++ src/frontend/script.ts
@@ -1,4 +1,5 @@
-/* tslint:disable:interface-name no-console no-var-requires space-before-function-paren */
+/* tslint:disable:interface-name no-console no-var-requires space-before-function-paren no-reference*/
+/// <reference path="./constants.ts" />
 
 type DirectSelect<E extends d3.BaseType> =
     d3.Selection<E, {}, HTMLElement, any>;
@@ -42,14 +43,6 @@ function InputJsonToGraph(j: InputJson): Graph {
     return { links, nodes };
 }
 
-function InitLinkSelection<E extends d3.BaseType>(
-    selectedSVG: DirectSelect<E>, links: GraphLink[]) {
-    const linkSelection = selectedSVG.append("g").attr("class", "links")
-        .selectAll("line").data(links).enter()
-        .append("line").attr("stroke-width", (d) => Math.sqrt(d.value));
-    return linkSelection;
-}
-
 function ElementAjacencyArray<E extends d3.BaseType, PE extends d3.BaseType, PD>(
     n: number, links: d3.Selection<E, GraphLink, PE, PD>): E[][] {
     const graph = Array<E[]>();
@@ -63,46 +56,6 @@ function ElementAjacencyArray<E extends d3.BaseType, PE extends d3.BaseType, PD>
     return graph;
 }
 
-function InitNodeDrag<E extends d3.BaseType, PE extends d3.BaseType, PD>(
-    node: d3.Selection<E, GraphNode, PE, PD>,
-    sim: d3.Simulation<GraphNode, GraphLink>) {
-    node.call(d3.drag<any, GraphNode>()
-        .on("start", (d: GraphNode) => {
-            if (!d3.event.active) {
-                sim.alphaTarget(0.3).restart();
-            }
-            d.fx = d.x;
-            d.fy = d.y;
-        })
-        .on("drag", (d: GraphNode) => {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-        })
-        .on("end", (d: GraphNode) => {
-            if (!d3.event.active) {
-                sim.alphaTarget(0);
-            }
-            d.fx = null;
-            d.fy = null;
-        }));
-}
-
-function InitNodeSelection<E extends d3.BaseType>(
-    selectedSVG: DirectSelect<E>, sim: d3.Simulation<GraphNode, GraphLink>, nodes: GraphNode[]) {
-    const circleNode = selectedSVG.append("g").attr("class", "nodes")
-        .selectAll("a").data(nodes).enter()
-        .append("a").attr("href", (d) => d.url).attr("target", "_blank")
-        .append("circle")
-        .attr("r", Const.kNodeRadius)
-        .attr("fill", (d) => Const.Color(d.group))
-        .attr("data-toggle", "tooltip")
-        .attr("title", (d) => d.name);
-
-    InitNodeDrag(circleNode, sim);
-    DeleteLinksFromTopics(circleNode);
-    return circleNode;
-}
-
 function DeleteLinksFromTopics<E extends d3.BaseType, PE extends d3.BaseType, PD>(
     nodes: d3.Selection<E, GraphNode, PE, PD>) {
     nodes.filter((d) => d.group === 0).each(function (d) {
@@ -112,80 +65,7 @@ function DeleteLinksFromTopics<E extends d3.BaseType, PE extends d3.BaseType, PD
     });
 }
 
-function NodeHoverEffect<E extends d3.BaseType, PE extends d3.BaseType, PD>(
-    nodes: d3.Selection<E, GraphNode, PE, PD>, aa: E[][]) {
-    const nodeHoverRadius = Const.kNodeRadius * Const.kRadiusExpand;
-    nodes.on("mouseover", function (d) {
-        d3.select(this)
-            .transition()
-            .duration(Const.kNodeTransDuration)
-            .attr("r", nodeHoverRadius);
-        d3.selectAll(aa[d.id])
-            .transition()
-            .duration(Const.kNodeTransDuration)
-            .style("stroke", "#111")
-            .style("stroke-width", "2px");
-    }).on("mouseout", function (d) {
-        d3.select(this)
-            .transition()
-            .duration(Const.kNodeTransDuration)
-            .attr("r", Const.kNodeRadius);
-        d3.selectAll(aa[d.id])
-            .transition()
-            .duration(Const.kNodeTransDuration)
-            .style("stroke", "#999")
-            .style("stroke-width", "1.5px");
-    });
-}
-
-function SetSimulationNodes(
-    sim: d3.Simulation<GraphNode, GraphLink>, nodes: GraphNode[]): void {
-    sim.nodes(nodes);
-}
-
-function SetSimulationLinks(
-    sim: d3.Simulation<GraphNode, GraphLink>, links: GraphLink[]): void {
-    const linkForce = sim.force<d3.ForceLink<GraphNode, GraphLink>>("link");
-    if (linkForce === undefined) {
-        console.error("get force simulation fail");
-        return;
-    }
-    linkForce.links(links).distance((l) => Const.kLinkDistance / l.value);
-}
-
 type CommonSelect<D> = d3.Selection<d3.BaseType, D, d3.BaseType, {}>;
-
-function ConnectModelAndView(
-    sim: d3.Simulation<GraphNode, GraphLink>,
-    nodes: CommonSelect<GraphNode>, links: CommonSelect<GraphLink>) {
-    sim.on("tick", () => {
-        links
-            .attr("x1", (d: any) => d.source.x)
-            .attr("y1", (d: any) => d.source.y)
-            .attr("x2", (d: any) => d.target.x)
-            .attr("y2", (d: any) => d.target.y);
-        nodes
-            .attr("cx", (d: any) => d.x)
-            .attr("cy", (d: any) => d.y);
-    });
-}
-
-function DrawForceGraph<E extends d3.BaseType>(
-    svg: DirectSelect<E>, simulation: d3.Simulation<GraphNode, GraphLink>, graph: Graph) {
-    const links = InitLinkSelection(svg, graph.links);
-    const nodes = InitNodeSelection(svg, simulation, graph.nodes);
-
-    const ajacencyArray = ElementAjacencyArray(nodes.size(), links);
-
-    NodeHoverEffect(nodes, ajacencyArray);
-
-    SetSimulationNodes(simulation, graph.nodes);
-    SetSimulationLinks(simulation, graph.links);
-
-    ConnectModelAndView(simulation, nodes, links);
-
-    EnableSVGTooltip();
-}
 
 function EnableSVGTooltip() {
     $("circle").tooltip({
@@ -196,17 +76,153 @@ function EnableSVGTooltip() {
     });
 }
 
-function CaptureSVGElemAndDraw() {
-    const svg = d3.select("svg.graph");
-    const [w, h] = [Number(svg.attr("width")), Number(svg.attr("height"))];
-    const simulation = InitForceSimulation([w / 2, h / 2]);
-    Const.InitSizes(w, h);
-    d3.json(svg.attr("config"), (error, json: InputJson) => {
-        if (error) {
-            throw error;
+class GraphVisualization {
+    public svg: d3.Selection<HTMLElement, {}, null, undefined>;
+    public simulation: d3.Simulation<GraphNode, GraphLink>;
+    public params: Const.Params;
+
+    constructor(svg: HTMLElement) {
+        this.svg = d3.select(svg);
+        this.simulation = InitForceSimulation(
+            [this.SVGWidth() / 2, this.SVGHeight() / 2]);
+        this.params = new Const.Params([this.SVGWidth(), this.SVGHeight()]);
+    }
+
+    public RequestAndDraw(): void {
+        const url = this.svg.attr("config");
+        d3.json(url, (error, json: InputJson) => {
+            if (error) {
+                throw error;
+            }
+            this.DrawForceGraph(InputJsonToGraph(json));
+        });
+    }
+
+    private DrawForceGraph(graph: Graph): void {
+        const links = this.GetLinkSelection(graph.links);
+        const nodes = this.GetNodeSelection(graph.nodes);
+
+        this.NodeHoverEffect(nodes, ElementAjacencyArray(nodes.size(), links));
+
+        this.SetSimulationNodes(graph.nodes);
+        this.SetSimulationLinks(graph.links);
+
+        this.ConnectModelAndView(nodes, links);
+
+        EnableSVGTooltip();
+    }
+
+    private GetLinkSelection(links: GraphLink[]) {
+        const linkSelection = this.svg.append("g").attr("class", "links")
+            .selectAll("line").data(links).enter()
+            .append("line").attr("stroke-width", (d) => Math.sqrt(d.value));
+        return linkSelection;
+    }
+
+    private GetNodeSelection(nodes: GraphNode[]) {
+        const circleNode = this.svg.append("g").attr("class", "nodes")
+            .selectAll("a").data(nodes).enter()
+            .append("a").attr("href", (d) => d.url).attr("target", "_blank")
+            .append("circle")
+            .attr("r", this.params.nodeRadius)
+            .attr("fill", (d) => Const.Color(d.group))
+            .attr("data-toggle", "tooltip")
+            .attr("title", (d) => d.name);
+
+        this.InitNodeDrag(circleNode);
+        DeleteLinksFromTopics(circleNode);
+        return circleNode;
+    }
+
+    private NodeHoverEffect<E extends d3.BaseType, PE extends d3.BaseType, PD>(
+        nodes: d3.Selection<E, GraphNode, PE, PD>, aa: E[][]) {
+        const params = this.params;
+        const nodeHoverRadius = params.nodeRadius * params.radiusExpand;
+        nodes.on("mouseover", function (d) {
+            d3.select(this)
+                .transition()
+                .duration(params.nodeTransDuration)
+                .attr("r", nodeHoverRadius);
+            d3.selectAll(aa[d.id])
+                .transition()
+                .duration(params.nodeTransDuration)
+                .style("stroke", "#111")
+                .style("stroke-width", "2px");
+        }).on("mouseout", function (d) {
+            d3.select(this)
+                .transition()
+                .duration(params.nodeTransDuration)
+                .attr("r", params.nodeRadius);
+            d3.selectAll(aa[d.id])
+                .transition()
+                .duration(params.nodeTransDuration)
+                .style("stroke", "#999")
+                .style("stroke-width", "1.5px");
+        });
+    }
+    private InitNodeDrag<E extends d3.BaseType, PE extends d3.BaseType, PD>(
+        node: d3.Selection<E, GraphNode, PE, PD>) {
+        node.call(d3.drag<any, GraphNode>()
+            .on("start", (d: GraphNode) => {
+                if (!d3.event.active) {
+                    this.simulation.alphaTarget(0.3).restart();
+                }
+                d.fx = d.x;
+                d.fy = d.y;
+            })
+            .on("drag", (d: GraphNode) => {
+                d.fx = d3.event.x;
+                d.fy = d3.event.y;
+            })
+            .on("end", (d: GraphNode) => {
+                if (!d3.event.active) {
+                    this.simulation.alphaTarget(0);
+                }
+                d.fx = null;
+                d.fy = null;
+            }));
+    }
+
+    private SetSimulationNodes(nodes: GraphNode[]) {
+        this.simulation.nodes(nodes);
+    }
+
+    private SetSimulationLinks(links: GraphLink[]) {
+        const linkForce =
+            this.simulation.force<d3.ForceLink<GraphNode, GraphLink>>("link");
+        if (linkForce === undefined) {
+            console.error("get force simulation fail");
+            return;
         }
-        DrawForceGraph(svg, simulation, InputJsonToGraph(json));
-    });
+        linkForce.links(links).distance(
+            (l) => this.params.linkDistance / l.value);
+    }
+
+    private ConnectModelAndView(
+        nodes: CommonSelect<GraphNode>, links: CommonSelect<GraphLink>) {
+        this.simulation.on("tick", () => {
+            links
+                .attr("x1", (d: any) => d.source.x)
+                .attr("y1", (d: any) => d.source.y)
+                .attr("x2", (d: any) => d.target.x)
+                .attr("y2", (d: any) => d.target.y);
+            nodes
+                .attr("cx", (d: any) => d.x)
+                .attr("cy", (d: any) => d.y);
+        });
+    }
+
+    private SVGWidth(): number {
+        return Number(this.svg.attr("width"));
+    }
+
+    private SVGHeight(): number {
+        return Number(this.svg.attr("height"));
+    }
+}
+
+function CaptureSVGElemAndDraw() {
+    new GraphVisualization($("svg.graph")[0]).RequestAndDraw();
 }
 
 CaptureSVGElemAndDraw();
